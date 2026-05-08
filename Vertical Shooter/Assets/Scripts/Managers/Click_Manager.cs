b0b2fb42e650be03652f982ac90cc990a71517cc
Tajinder <tnijjar2@my.centennialcollege.ca>
2026-05-08 14:28:55 -0400

    Add basic pause menu

--- Vertical Shooter/Assets/Scripts/Managers/Click_Manager.cs
+++ Vertical Shooter/Assets/Scripts/Managers/Click_Manager.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Click_Manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject pausePanel;
+
+
+    public void OnPause(InputAction.CallbackContext context)
     {
-        
+
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+            return;
+        } else
+        {
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResumeGame()
     {
-        
+        Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
